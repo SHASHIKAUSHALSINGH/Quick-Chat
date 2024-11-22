8f85b4d6618ea7c630e7a9e46c38bb1b2ff73899
Shashi Kaushal Singh <121636279+SHASHIKAUSHALSINGH@users.noreply.github.com>
2024-11-23 04:00:25 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -7,6 +7,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://quick-chat-2byme.onrender.com/api",
   withCredentials: true,
 });
