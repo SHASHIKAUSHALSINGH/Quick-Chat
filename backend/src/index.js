b9e4e08b4d034dffb500d8f234e7e468f855508b
Shashi Kaushal Singh <121636279+SHASHIKAUSHALSINGH@users.noreply.github.com>
2024-12-24 00:58:05 +0530

    Update index.js
    change cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,11 +23,11 @@ const PORT =process.env.PORT
 const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({
-//     origin:"https://quick-chat-hbtu.vercel.app",
-//     credentials:true
-// }))
-app.use(cors())
+app.use(cors({
+    origin:"https://deft-douhua-986e4f.netlify.app",
+    credentials:true
+}))
+// app.use(cors())
 app.get("/", (req, res) => {
     res.send("Server is up and running!");
 });
