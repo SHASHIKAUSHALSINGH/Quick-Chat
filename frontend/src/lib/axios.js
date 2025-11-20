90c7f893410a22992192ec7dd6aa74989385b9e1
Shashi Kaushal Singh <121636279+SHASHIKAUSHALSINGH@users.noreply.github.com>
2025-11-21 00:20:16 +0530

    Update axios baseURL for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -7,6 +7,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://quick-chat-2byme.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://deft-douhua-986e4f.netlify.app/api",
   withCredentials: true,
 });
